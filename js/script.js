d8383f859a725b77d1245ec02af24aea29906e87
Billy King <billyjack30@gmail.com>
2021-03-18 13:51:11 -0400

    added event listeners to the player's choice

--- js/script.js
+++ js/script.js
@@ -2,18 +2,34 @@
 function computerPlay() {
     let play = random();
         if (play === 0) {
-            compterDecision = "Rock";
+            compterDecision = 'Rock';
         } else if (play === 1) {
-            compterDecision = "Paper";
+            compterDecision = 'Paper';
         } else {
-            compterDecision = "Scissors";
+            compterDecision = 'Scissors';
         }
     console.log(compterDecision);
 }
 
-
 function random() {
    return Math.floor(Math.random() * 3);
+}
+
+let choiceButtonRock = document.getElementById('isRock');
+let choiceButtonPaper = document.getElementById('isPaper');
+let choiceButtonScissors = document.getElementById('isScissors');
+let playerChoice = '';
+
+choiceButtonRock.onclick = function() {
+    playerChoice = 'Rock';
+}
+
+choiceButtonPaper.onclick = function() {
+    playerChoice = 'Paper';
+}
+
+choiceButtonScissors.onclick = function() {
+    playerChoice = 'Scissors';
 }
 
 for (i = 0; i < 10; i++) {
