677770773bc0b86effffcd08edb0d23fd13d74b8
Billy King <billyjack30@gmail.com>
2021-03-19 10:22:32 -0400

    Added a function for comparing computer and player and calculating a winner

--- js/script.js
+++ js/script.js
@@ -15,10 +15,53 @@ function random() {
    return Math.floor(Math.random() * 3);
 }
 
+function playGame() {
+    switch (playerChoice, computerChoice) {
+        case 'Rock', 'Rock':
+            result = 'It\'s a tie!';
+            break;
+        case 'Rock', 'Paper':
+            result = 'You lost!';
+            computerScore += 1;
+            break;
+        case 'Rock', 'Scissors':
+            result = 'You win!';
+            playerScore += 1;
+            break;
+        case 'Paper', 'Rock':
+            result = 'You win!';
+            playerScore += 1;
+            break;
+        case 'Paper', 'Paper':
+            result = 'It\'s a tie!';
+            break;
+        case 'Paper', 'Scissors':
+            result = 'You lost!';
+            computerScore += 1;
+            break;
+        case 'Scissors', 'Rock':
+            result = 'You lost!';
+            computerScore += 1;
+            break;
+        case 'Scissors', 'Paper':
+            result = 'You win!';
+            playerScore += 1;
+            break;
+        case 'Scissors', 'Scissors':
+            result = 'It\'s a tie';
+            break;
+    }
+    console.log(result);
+}
+
 let choiceButtonRock = document.getElementById('isRock');
 let choiceButtonPaper = document.getElementById('isPaper');
 let choiceButtonScissors = document.getElementById('isScissors');
 let playerChoice = '';
+let result;
+let computerChoice = 'Rock';
+let computerScore = 0;
+let playerScore = 0;
 
 choiceButtonRock.onclick = function() {
     playerChoice = 'Rock';
